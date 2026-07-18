3376ac06343ce79d0308ecf4370d73a8737abe52
UseMil <hq.usemil@gmail.com>
2026-07-18 05:40:31 -0700

    time update

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const weddingConfig = {
     brideName: "Bint-e-Mubashar",
     groomName: "Muhammad Mubeen Muzamil",
-    weddingDate: "August 8, 2026 18:00:00",
+    weddingDate: "August 8, 2026 19:00:00",
     venue: "Al Rehman Marquee, Sahiwal",
     musicSrc: "assets/music/wedding-song.mp3",
     mapLink: "https://maps.app.goo.gl/HNBszW4PcnLvjjHH7", // <--- Paste your new link right here
